37affc5d77c70aa5989a45b551b1d681f1d47abf
napikep <kemalnapi2@gmail.com>
2025-09-08 16:25:58 +0700

    Update kevnigma.sh

--- kevnigma.sh
+++ kevnigma.sh
@@ -1,3 +1,38 @@
+#!/bin/bash
+################################################################################
+#                                                                              #
+# Copyright (c) 2023 Díwash (Diwash0007)                                       #
+#                                                                              #
+# Licensed under the Apache License, Version 2.0 (the "License");              #
+# you may not use this file except in compliance with the License.             #
+# You may obtain a copy of the License at                                      #
+#                                                                              #
+#     http://www.apache.org/licenses/LICENSE-2.0                               #
+#                                                                              #
+# Unless required by applicable law or agreed to in writing, software          #
+# distributed under the License is distributed on an "AS IS" BASIS,            #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.     #
+# See the License for the specific language governing permissions and          #
+# limitations under the License.                                               #
+#                                                                              #
+################################################################################
+
+# ***************************************************************************************
+# - Script to install Chrome Remote Desktop and a Desktop Environment.
+# - Downloads and installs Chrome Remote Desktop and an 'xfce' Desktop Environment.
+# - Sends Remote Desktop at https://remotedesktop.google.com/access to access the 'xfce' Instance.
+# - Author: Díwash (Diwash0007)
+# - Version: generic:1.0
+# - Date: 19 June 2023
+#
+#       * Changes for V0.99 (20230619) - make it clear that 'xfce' Desktop Environment is not ready.
+#       * Changes for V1.0 (20230620) - make it clear that 'xfce' desktop environment is now ready.
+#
+# ***************************************************************************************
+
+#######################################################################
+# Extra functions needed during further operations
+# print message and quit
 abort() {
   echo "$@";
   exit 1;
@@ -68,5 +103,6 @@ WorkNow() {
 WorkNow "$@";
 
 # --- end main() ---
+
 
 
